7aff42d18336160af62379015159bdbbcbe0f8f2
David Goldfarb <deg@degel.com>
2013-10-14 14:54:25 +0300

    Use latest dependencies.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject my-muxx-sites "0.1.0"
+(defproject my-muxx-sites "0.1.1-SNAPSHOT"
   :description "Deployment project to wrap my web apps into a single site."
   :url "https://github.com/deg/my-muxx-sites"
   :license {:name "Eclipse Public License"
@@ -19,11 +19,14 @@
                  [org.clojure/clojure "1.5.1"]
 
                  ;; Degel's Clojure utility library
-                 [degel-clojure-utils "0.1.6"]
+                 [degel-clojure-utils "0.1.14-SNAPSHOT"]
+
+                 ;; Degel's website multiplexer
+                 [muxx "0.1.1-SNAPSHOT"]
 
                  ;; Our apps
-                 [deg-scraps "0.1.0"]
-                 [webol "0.1.0"]]
+                 [deg-scraps "0.1.1-SNAPSHOT"]
+                 [webol "0.1.2-SNAPSHOT"]]
 
   :profiles {:dev
              { :plugins [[lein-marginalia "0.7.1"]]}}

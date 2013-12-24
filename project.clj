831dfe76b1bacc13800cbb101fd082a5bf6aa564
David Goldfarb <deg@degel.com>
2013-12-24 19:58:34 +0200

    0.1.4 - Latest dependencies

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject degel/my-muxx-sites "0.1.3"
+(defproject degel/my-muxx-sites "0.1.4"
   :description "Deployment project to wrap my web apps into a single site."
   :url "https://github.com/deg/my-muxx-sites"
   :license {:name "Eclipse Public License"
@@ -18,19 +18,20 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
 
                  ;; Degel Clojure utility library
-                 [degel/degel-clojure-utils "0.1.17" :exclusions [org.clojure/tools.reader]]
+                 [degel/degel-clojure-utils "0.1.20"]
 
                  ;; Degel website multiplexer
-                 [degel/muxx "0.1.4"]
+                 [degel/muxx "0.1.5"]
 
                  ;; Our apps
-                 [degel/mummy "0.1.0"]
-                 [deg-scraps "0.1.2"]
-                 [degel/webol "0.1.7"]]
+                 [degel/mummy "0.1.1"]
+                 [deg-scraps "0.1.3"]
+                 [degel/webol "0.1.8"]]
 
   :profiles {:dev
              { :plugins [[lein-marginalia "0.7.1"]]}}
 
   :min-lein-version "2.0.0"
+  ;:pedantic? :abort
 
   :main degel.deploy.deployment)
